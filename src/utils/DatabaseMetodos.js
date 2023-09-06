23e5ae65673a1463dbb4b9ef901104d89596d922
wallace-lentes <wallace.lentes@gmail.com>
2023-09-06 12:10:32 -0300

    ci: adição do metodo de atualização.

--- src/utils/DatabaseMetodos.js
+++ src/utils/DatabaseMetodos.js
@@ -10,6 +10,12 @@ class DatabaseMetodos{
     static buscarPorId(entidade, id){
         return Database[entidade][id]
     }
+    static deletarPorId(entidade, id){
+        delete Database[entidade][id]
+    }
+    static atualizarPorId(entidade, id, data){
+        Database[entidade][id] = data
+    }
 }
 
 export default DatabaseMetodos;
