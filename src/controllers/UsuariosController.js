6695b3c5bb030416bb6829b2258c6a66322ae6a8
wallace-lentes <wallace.lentes@gmail.com>
2023-09-06 12:04:15 -0300

    ci: rota de atualização para usuarios adicionado.

--- src/controllers/UsuariosController.js
+++ src/controllers/UsuariosController.js
@@ -1,5 +1,6 @@
-import UsuariosMetodos from "../utils/UsusariosMetodos.js";
 import UsuariosModel from "../models/UsuariosModel.js";
+import ValidacaoServices from "../services/ValidacaoServices.js";
+import UsuariosMetodos from "../utils/UsuariosMetodos.js";
 
 class UsuariosController {
   /**
@@ -15,10 +16,27 @@ class UsuariosController {
       res.status(200).json(usuarios)
     })
 
-    app.get("/usuarios/:nome", (req, res) => {
+    /**
+    * Rota para buscar usuários pelo id
+    */
+    app.get("/usuarios/:id", (req, res) => {
+      const id = req.params.id
+      const isValid = ValidacaoServices.validarExistencia(id)
+      if (isValid) {
+        const resposta = UsuariosMetodos.buscarUsuarioPorId(id)
+        res.status(200).json(resposta)
+      }
+      res.status(404).json({ error: true, message: `Usuário não encontrado para o id ${id}`})
+    })
+
+    app.delete("/usuarios/:id", (req, res) => {
       const id = req.params.nome
-      const resposta = UsuariosMetodos.buscarUsuarioPorId(id)
-      res.status(200).json(resposta)
+      const isValid = ValidacaoServices.validarExistencia(id)
+      if (isValid) {
+        UsuariosMetodos.deletarUsuarioPorId(id)
+        res.status(200).json({ error: false })
+      }
+      res.status(404).json({ error: true, message: `Usuario não encontrado para o id ${id}`})
     })
 
     /**
@@ -26,12 +44,32 @@ class UsuariosController {
      */
     app.post("/usuarios", (req, res) => {
       const body = Object.values(req.body)
-      const usuario = new UsuariosModel(...body)
-      UsuariosMetodos.inserirUsuarios(usuario)
-      res.status(200).json({
-        error: false,
-        message: "Usuario criado com sucesso!"
-      })
+      const isValid = ValidacaoServices.validaCamposUsuario(...body)
+      if(isValid){
+        const usuarioModelado = new UsuariosModel(...body)
+        UsuariosMetodos.inserirUsuario(usuarioModelado)
+        res.status(200).json({
+          error: false,
+          message: "Usuario criado com sucesso!"
+        })
+      }
+      res.status(400).json({ error: true, message: `Campos invalidos`})
+    })
+
+    app.put("/usuarios/:id", (req, res) => {
+      const id = req.params.id
+      const body = req.body
+      const exists = ValidacaoServices.validarExistencia(id)
+      const isValid = ValidacaoServices.validaCamposUsuario(body.nome, body.email, body.telefone)
+      if(exists){
+        if(isValid){
+          const usuarioModelado = new UsuariosModel(body.nome, body.email, body.telefone)
+          UsuariosMetodos.AtualizarUsuarioPorId(id, usuarioModelado)
+          res.status(204).json()
+        }
+        res.status(400).json({ error: true, message: `Campos invalidos`})
+      }
+      res.status(404).json({ error: true, message: `Usuario não encontrado para o id ${id}`})
     })
   }
 }
