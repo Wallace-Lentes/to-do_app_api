d37d5219f87b88dcc8f40b5f4891048015b1fa7e
wallace-lentes <wallace.lentes@gmail.com>
2023-09-06 11:54:52 -0300

    build: mudança no nome do arquivo e criaçao do metodo de atualização.

---
+++ src/utils/UsuariosMetodos.js
@@ -0,0 +1,24 @@
+import DatabaseMetodos from "../database/DatabaseMetodos.js";
+import UsuariosModel from "../models/UsuariosModel.js";
+
+const USUARIOS_TABELA = "Usuarios"
+
+class UsuariosMetodos extends DatabaseMetodos{
+    inserirUsuario(data){
+        this.inserir(USUARIOS_TABELA, data)
+    }
+    static buscarTododsOsUsuarios(){
+        return this.buscar(USUARIOS_TABELA)
+    }
+    static buscarUsuarioPorId(id){
+        return this.buscarPorId(USUARIOS_TABELA, id)
+    }
+    static deletarUsuarioPorId(id){
+        return this.deletarPorId(USUARIOS_TABELA, id)
+    }
+    static AtualizarUsuarioPorId(id, data){
+        this.atualizarPorId(USUARIOS_TABELA, id, data)
+    }
+}
+
+export default UsuariosMetodos;
