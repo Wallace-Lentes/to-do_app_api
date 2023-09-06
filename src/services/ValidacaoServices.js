8ad13e03ea20319881106cf8501d5e790e99fc70
wallace-lentes <wallace.lentes@gmail.com>
2023-09-06 12:08:43 -0300

    ci: validação de campos dos usuarios adicionado.

--- src/services/ValidacaoServices.js
+++ src/services/ValidacaoServices.js
@@ -1,4 +1,4 @@
-import UsuariosMetodos from "../utils/UsusariosMetodos";
+import UsuariosMetodos from "../utils/UsuariosMetodos.js";
 
 class ValidacaoServices{
     static validarExistencia(id){
@@ -8,6 +8,20 @@ class ValidacaoServices{
         }else{
             return false
         }
+    }
+    static validaNome(nome){
+        return typeof nome == "string" && nome.length > 2
+    }
+    static validaEmail(email){
+        return typeof email == "string" && email.length > 2
+    }
+    static validaTelefone(telefone){
+        const telefoneInt = parseInt(telefone)
+        return typeof telefone == "string" && telefone.length > 9 && telefone == telefoneInt
+    }
+    static validaCamposUsuario(nome, email, telefone){
+        const isValid = this.validaNome(nome) && this.validaEmail(email) && this.validaTelefone(telefone)
+        return isValid
     }
 }
 export default ValidacaoServices
