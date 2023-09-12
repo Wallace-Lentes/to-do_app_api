7124aab0bf0bd850633215691d85b3d3841869e1
wallace-lentes <wallace.lentes@gmail.com>
2023-09-12 11:34:12 -0300

    build: post atualizado para inserçao de usuarios.

--- src/controllers/UsuariosController.js
+++ src/controllers/UsuariosController.js
@@ -1,6 +1,6 @@
 import UsuariosModel from "../models/UsuariosModel.js";
 import ValidacaoServices from "../services/ValidacaoServices.js";
-import UsuariosMetodos from "../utils/UsuariosMetodos.js";
+import UsuariosDAO from "../DAO/UsuariosDAO.js";
 
 class UsuariosController {
   /**
@@ -11,8 +11,9 @@ class UsuariosController {
     /**
      * Rota para buscar todos os usuários
      */
-    app.get("/usuarios", (req, res) => {
-      const usuarios = UsuariosMetodos.buscarTododsOsUsuarios()
+    app.get("/usuarios", async (req, res) => {
+      const usuarios = await UsuariosDAO.buscarTododsOsUsuarios()
+      console.log(usuarios)
       res.status(200).json(usuarios)
     })
 
@@ -23,37 +24,42 @@ class UsuariosController {
       const id = req.params.id
       const isValid = ValidacaoServices.validarExistencia(id)
       if (isValid) {
-        const resposta = UsuariosMetodos.buscarUsuarioPorId(id)
+        const resposta = UsuariosDAO.buscarUsuarioPorId(id)
         res.status(200).json(resposta)
       }
-      res.status(404).json({ error: true, message: `Usuário não encontrado para o id ${id}`})
+      res.status(404).json({ error: true, message: `Usuário não encontrado para o id ${id}` })
     })
 
     app.delete("/usuarios/:id", (req, res) => {
       const id = req.params.nome
       const isValid = ValidacaoServices.validarExistencia(id)
       if (isValid) {
-        UsuariosMetodos.deletarUsuarioPorId(id)
+        UsuariosDAO.deletarUsuarioPorId(id)
         res.status(200).json({ error: false })
       }
-      res.status(404).json({ error: true, message: `Usuario não encontrado para o id ${id}`})
+      res.status(404).json({ error: true, message: `Usuario não encontrado para o id ${id}` })
     })
 
     /**
      * Rota apra inserir um novo usuário
      */
-    app.post("/usuarios", (req, res) => {
+    app.post("/usuarios", async (req, res) => {
       const body = Object.values(req.body)
       const isValid = ValidacaoServices.validaCamposUsuario(...body)
       if(isValid){
         const usuarioModelado = new UsuariosModel(...body)
-        UsuariosMetodos.inserirUsuario(usuarioModelado)
-        res.status(200).json({
-          error: false,
-          message: "Usuario criado com sucesso!"
-        })
+        try {
+          await UsuariosDAO.inserirUsuario(usuarioModelado)
+          res.status(201).json({
+            error: false,
+            message: "Usuario criado com sucesso!"
+          })
+        } catch (error) {
+          res.status(503).json({ error: true, message: `Servidor indisponivel no momento.` })
+        }
+      }else{
+        res.status(400).json({ error: true, message: `Campos invalidos` })
       }
-      res.status(400).json({ error: true, message: `Campos invalidos`})
     })
 
     app.put("/usuarios/:id", (req, res) => {
@@ -61,15 +67,15 @@ class UsuariosController {
       const body = req.body
       const exists = ValidacaoServices.validarExistencia(id)
       const isValid = ValidacaoServices.validaCamposUsuario(body.nome, body.email, body.telefone)
-      if(exists){
-        if(isValid){
+      if (exists) {
+        if (isValid) {
           const usuarioModelado = new UsuariosModel(body.nome, body.email, body.telefone)
-          UsuariosMetodos.AtualizarUsuarioPorId(id, usuarioModelado)
+          UsuariosDAO.AtualizarUsuarioPorId(id, usuarioModelado)
           res.status(204).json()
         }
-        res.status(400).json({ error: true, message: `Campos invalidos`})
+        res.status(400).json({ error: true, message: `Campos invalidos` })
       }
-      res.status(404).json({ error: true, message: `Usuario não encontrado para o id ${id}`})
+      res.status(404).json({ error: true, message: `Usuario não encontrado para o id ${id}` })
     })
   }
 }
